8775e93ea7cbadc5affbc1031cf35cb3fc7448c0
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2023-11-10 01:40:26 +0100

    changed the lastest rust solution a bit

--- rust/src/length_of_last_word.rs
+++ rust/src/length_of_last_word.rs
@@ -1,7 +1,7 @@
 pub fn length_of_last_word(s: String) -> i32 {
     let mut num_of_words:i32 = 0;
     for c in s.chars().rev() {
-        if c != " ".chars().next().unwrap() {
+        if c != ' ' {
             num_of_words += 1;
         } else if num_of_words > 0 {
             return num_of_words; 
